a743838d79b1679eea90aa0958abaacffe161b2f
shivam <shivamdatta465@gmail.com>
2021-11-12 22:17:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+console.time('code took ')
+console.log("hello");
+console.log(4+34);
+console.log(true);
+console.log([34,2,1,2]);
+console.log({shivam:'this',marks:34});
+// console.log(table);
+console.table({shivam:'this',marks:34});
+console.warn('warning');
+// console.clear()
+console.timeEnd('code took ')
+let age=200
+console.assert(age<189,'overage')
+console.error('error');

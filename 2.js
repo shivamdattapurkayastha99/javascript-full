6659d3ed369955deb37f31f6433ccf9586d892fe
shivam <shivamdatta465@gmail.com>
2021-11-15 22:48:53 +0530

    first commit

---
+++ 2.js
@@ -0,0 +1,22 @@
+console.log("shivam")
+var name="shivam";
+console.log(name);
+var x="shivam datta";
+console.log(name,x);
+var marks=100;
+var city='kolkata'
+console.log(city);
+const myname='Datta'
+console.log(name);
+const fruit='apple';
+{
+    let city='Kanpur';
+    city='kolkata';
+    console.log(city);
+
+
+}
+const arr1=[12,23,45]
+arr1=[23,45,56]
+console.log(arr1);
+

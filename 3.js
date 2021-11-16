4586ecf2841075afcd2ecf823c43b0b90bb1e970
shivam <shivamdatta465@gmail.com>
2021-11-16 17:57:08 +0530

    first commit

---
+++ 3.js
@@ -0,0 +1,28 @@
+let name="shivam";
+console.log(name);
+console.log(typeof(name));
+let marks=100
+console.log(typeof(marks));
+let isDriver=true;
+console.log(typeof(isDriver));
+let nullVar=true;
+console.log(typeof(nullVar));
+let undef=undefined;
+console.log(typeof(undef));
+myarr=[1,2,3,4,"shivam"]
+console.log(typeof(myarr));
+let setmarks={
+    shivam:12,
+    datta:14,
+}
+console.log(setmarks);
+console.log(typeof(setmarks));
+function findName() {
+    
+}
+
+
+console.log(typeof(findName));
+let date=new Date();
+console.log(typeof(date));
+

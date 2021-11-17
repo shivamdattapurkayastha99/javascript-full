9e1cc93b19bb0b54f1c18cd8271894f026bb1360
shivam <shivamdatta465@gmail.com>
2021-11-17 22:09:09 +0530

    second commit

---
+++ 4.js
@@ -0,0 +1,23 @@
+console.log('shivam');
+let a=34;
+console.log(a,typeof(a));
+a=String(34)
+console.log(a,typeof(a));
+let booleanVar=true;
+console.log(booleanVar,typeof(booleanVar));
+let date=new Date();
+console.log(date,typeof(date));
+let arr=String([1,2,3,4]);
+console.log(arr,typeof(arr));
+let i=25
+console.log(i.toString())
+let stri="3434"
+stri=Number("343d4")
+stri=Number(false)
+let number=Number('34.098');
+console.log(number);
+console.log(stri,typeof(stri));
+console.log(number.toFixed(2),(typeof(number)));
+
+
+

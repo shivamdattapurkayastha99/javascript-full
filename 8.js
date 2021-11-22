91c81161870fbcb88e6968ae0bf9f711e41a29e8
shivam <shivamdatta465@gmail.com>
2021-11-22 18:56:32 +0530

    second commit

---
+++ 8.js
@@ -0,0 +1,41 @@
+// console.log('HELLO');
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+    
+// }
+
+// let j=0
+// while (j<100) {
+//     console.log(i);
+//     j++;
+
+// }
+// let k=0
+// do {
+//     console.log(k+1);
+//     if (k==5) {
+//         k+=1
+//         continue
+
+
+//     }
+//     k+=1
+
+// } while (k<10);
+
+// console.log("done");
+let arr=[2,5,6,4,2,3]
+for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    console.log(element);
+}
+let obj={
+    name:"Shivam",
+
+    age:22,
+
+}
+for (let key in obj) {
+    console.log(`${key} ${obj[key]}`);
+    
+}
